f6e89307591c14a4ab6cd2b6c1f70bee52aa07fd
Lucke0302 <lucke0302@gmail.com>
2024-08-24 20:30:36 -0300

    port

--- view/componentes/menu/menu.php
+++ view/componentes/menu/menu.php
@@ -1,10 +1,10 @@
 <nav id="nav">
     <ul>
-    <li><a id="menu"><span id="s1"></span><span id="s2"></span><span id="s3"></span></a></li>
-    <li><a href=”#”>pagina 1</a></li>
-    <li><a href=”#”>pagina 2</a></li>
-    <li><a href=”#”>pagina 3</a></li>
-    <li><a href=”#”>pagina 4</a></li>
-    <li><a href=”#”>pagina 5</a></li>
+        <li><a id="menu" class="button"><span id="s1" class="span"></span><span id="s2" class="span"></span><span id="s3" class="span"></span></a></li>
+        <li><a href=”#”>pagina 1</a></li>
+        <li><a href=”#”>pagina 2</a></li>
+        <li><a href=”#”>pagina 3</a></li>
+        <li><a href=”#”>pagina 4</a></li>
+        <li><a href=”#”>pagina 5</a></li>
     </ul>
 </nav>
--- view/script.js
+++ view/script.js
@@ -1,8 +1,40 @@
 s1 = document.getElementById("s1");
+s2 = document.getElementById("s2");
 s3 = document.getElementById("s3");
 nav = document.getElementById("nav");
+menu = document.getElementById("menu");
 
-nav.addEventListener('mouseenter', function() {
+nav.addEventListener('click', function(){
+    c1 = menu.className;
+    console.log(c1);
+    if(c1 == "button"){
+        menu.className = "button-active";
+        nav.style.marginLeft = "0px";
+        s1.style.rotate = "-360deg";
+        s3.style.rotate = "-360deg";
+        menu.style.marginRight = "-80%";
+        s1.style.backgroundColor = "white";
+        s2.style.backgroundColor = "white";
+        s3.style.backgroundColor = "white";
+    }
+    else{
+        menu.className = "button";
+        s1.style.rotate = "0deg";
+        s3.style.rotate = "0deg";
+        menu.style.marginRight = "-120%";
+        s1.style.backgroundColor = "#164c45";
+        s2.style.backgroundColor = "#164c45";
+        s3.style.backgroundColor = "#164c45";
+        if(window.innerWidth  <= 1024){
+            nav.style.marginLeft = "-72vw";
+        }
+        else{
+            nav.style.marginLeft = "-16vw";
+        }
+    }
+});
+
+/*nav.addEventListener('mouseenter', function() {
     
     s1.style.rotate = "140deg";
     s1.style.marginLeft = "-5px";
@@ -20,4 +52,4 @@ nav.addEventListener('mouseleave', function() {
     s3.style.rotate = "360deg";
     s3.style.marginLeft = "0px";
     s3.style.width = "inherit";
-});
+});*/
